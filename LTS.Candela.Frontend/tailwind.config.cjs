3734b5483422bd7278e3a36378eb46bad11ff0df
Juan Carlos Candela <jccandelabordera@gmail.com>
2025-06-19 01:03:34 +0200

    Fix TailwindCss

---
+++ LTS.Candela.Frontend/tailwind.config.cjs
@@ -0,0 +1,11 @@
+import PrimeUI from 'tailwindcss-primeui';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [PrimeUI],
+}
