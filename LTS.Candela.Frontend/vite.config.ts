98be813aefae6fdf2e00bfaf7ecc513de17f1803
Juan Carlos Candela <jccandelabordera@gmail.com>
2025-06-19 00:17:32 +0200

    Use CDN for icons

--- LTS.Candela.Frontend/vite.config.ts
+++ LTS.Candela.Frontend/vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
+resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
