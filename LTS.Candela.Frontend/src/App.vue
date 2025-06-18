df08041b1fb8ba32eeffaffd3a5e22eea52c7934
Juan Carlos Candela <jccandelabordera@gmail.com>
2025-06-19 00:20:01 +0200

    Fix Toast

--- LTS.Candela.Frontend/src/App.vue
+++ LTS.Candela.Frontend/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="min-h-screen bg-background">
+    <Toast />
     <main class="container mx-auto py-8">
       <router-view></router-view>
     </main>
@@ -7,6 +8,7 @@
 </template>
 
 <script setup lang="ts">
+import Toast from 'primevue/toast';
 // Main app component
 </script>
 
