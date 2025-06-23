368641174c448000ae5607a35c97af0b819b3b8d
Juan Carlos CANDELA <juan.carlos.candela@cotecna.es>
2025-06-23 09:26:55 +0200

    Update migrations for postgresql

--- LTS.Candela.API/LTS.Candela.API/Migrations/20250623072633_LatestChanges.Designer.cs
+++ LTS.Candela.API/LTS.Candela.API/Migrations/20250623072633_LatestChanges.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LTS.Candela.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace LTS.Candela.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250623072633_LatestChanges")]
+    partial class LatestChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
